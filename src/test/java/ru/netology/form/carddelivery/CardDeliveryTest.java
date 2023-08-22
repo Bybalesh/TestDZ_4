b0b8b04629b3703cb9ddcf4e449e791b61e7b54c
Nikita Kruglov <bybalesh234@gmail.com>
2023-08-22 22:05:21 +0600

    Completed test 2

--- src/test/java/ru/netology/form/carddelivery/CardDeliveryTest.java
+++ src/test/java/ru/netology/form/carddelivery/CardDeliveryTest.java
@@ -8,11 +8,10 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.attribute;
-import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import static org.apache.commons.lang3.StringUtils.stripStart;
+
 
 
 public class CardDeliveryTest {
@@ -40,28 +39,23 @@ public class CardDeliveryTest {
     @Test
     public void shouldBeSuccessTimeCompleted(){
         open("http://localhost:9999");
-        $("[data-test-id=city] input").setValue("Волгоград");
+            String planningDate = generateDate(7, "dd.MM.yyyy");
 
-        int period = 7;
-        String date = generateDate(period, "dd.MM.yyyy");
-        String day = generateDate(period, "dd");
-        String month = generateDate(period, "MM");
-        String currentMonth = generateDate(0, "MM");
-        $("[data-test-id='date']").click();
-        if (!generateDate(3, "MM").equals(generateDate(7, "MM"))) {
-            $$(".calendar__arrow_direction_right").findBy(attribute("data-step", "1")).click();
-            $$(".calendar__day").find(exactText(stripStart(day, "0"))).click();
-        } else {
-            $$(".calendar__day").find(exactText(stripStart(day, "0"))).click();
+            $("[data-test-id=city] input").setValue("Ка");
+            $(".popup__inner").shouldBe(exist, Duration.ofSeconds(5));
+            $$(".menu-item").find(text("Казань")).click();
+            $("button span.icon_name_calendar").click();
+            $("div.calendar-input__calendar-wrapper").shouldBe(visible, Duration.ofSeconds(40));
+            if (!generateDate(3, "MM").equals(generateDate(7, "MM"))) {
+                $(".popup [data-step='1']").click();
+            }
+            $$(".calendar__day").find(text(generateDate(7, "d"))).click();
+            $("[data-test-id=name] input").setValue("Петров Петр Петрович");
+            $("[data-test-id=phone] input").setValue("+79136698749");
+            $(".checkbox__box").click();
+            $(".button").click();
+            $("[data-test-id=notification] .notification__title").shouldHave(exactText("Успешно!"), Duration.ofSeconds(20));
+            $("[data-test-id=notification] .notification__content").shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
         }
-        $("[data-test-id='name'] input").setValue("Петров Петр Петрович");
-        $("[data-test-id='phone'] input").setValue("+78567324855");
-        $("[data-test-id='agreement']").click();
-        $("button.button").click();
-        $(".notification__content")
-                .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + date));
-
-    }
 
 }
